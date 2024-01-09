31c1863130a9083693180224cc1ed56785be3c2d
giahypham <andygpham@gmail.com>
2024-01-08 23:07:39 -0500

    sets up scripts dependencies and server

---
+++ data/migrations/20240109031533_initial-migration.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+
+    })
+    .createTable('resources', table => {
+
+    })
+    .createTable('tasks', table => {
+
+    })
+    .createTable('project_resources', table => {
+
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
