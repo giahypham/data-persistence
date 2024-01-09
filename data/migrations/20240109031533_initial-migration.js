6eb78fdfc6b01b02a3567b4edd592704dedff516
giahypham <andygpham@gmail.com>
2024-01-09 12:39:57 -0500

    Sets up Migrations'

--- data/migrations/20240109031533_initial-migration.js
+++ data/migrations/20240109031533_initial-migration.js
@@ -5,16 +5,47 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', table => {
-
+        table.increments('project_id').primary()
+        table.string('project_name', 200).notNullable()
+        table.string('project_description', 200)
+        table.integer('project_completed').defaultTo(0)
     })
     .createTable('resources', table => {
-
+        table.increments('resource_id').primary()
+        table.string('resource_name', 200).notNullable().unique()
+        table.string('resource_description', 200)
     })
     .createTable('tasks', table => {
+        table.increments('task_id').primary()
+        table.string('task_description', 200).notNullable()
+        table.string('task_notes', 200)
+        table.integer('task_completed').defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
 
     })
     .createTable('project_resources', table => {
-
+        table.increments('project_resource_id').primary()
+        table.string('project_resource_description', 200)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
 };
 
